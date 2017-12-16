ba3a4c100338ced31e79b9794eb742fdb3f8dfe1
jlasanen <jukka-pekka.lasanen@teliacompany.com>
2017-12-16 11:33:01 +0200

    QueryString NullPointerException fix

--- sample_applications/http_adapter/src/smsServer/QueryString.java
+++ sample_applications/http_adapter/src/smsServer/QueryString.java
@@ -36,21 +36,33 @@ public class QueryString {
      * returns true if query string contains the key
      */
     public boolean has(String key) {
-        return parameters.containsKey(key);
+        try {
+            return parameters.containsKey(key);
+        } catch (NullPointerException ex) {
+            return false;
+        }
     }
     
     /*
      * returns a single value key value
      */
     public String getParam(String key) {
-        return parameters.get(key).get(0);
+        try {
+            return parameters.get(key).get(0);
+        } catch (NullPointerException ex) {
+            return null;
+        }
     }
     
     /*
      * returns a multivalue key value as an array
      */
     public String[] getParamList(String key) {
-        return parameters.get(key).toArray(new String[0]);
+        try {
+            return parameters.get(key).toArray(new String[0]);
+        } catch (NullPointerException ex) {
+            return null;
+        }
     }
 
     // = end of public part ===============================================
@@ -63,21 +75,30 @@ public class QueryString {
      */
     private HashMap<String, List<String>> parseQuery(String queryString) throws UnsupportedEncodingException {
         HashMap <String, List<String>> params = new HashMap <String, List<String>>();
-        Log.logDebug("query string: " + queryString);
+        Log.logDebug("query string: " + (queryString != null ? queryString : "no query string"));
+        if (queryString == null || queryString.length() == 0) {
+            // no query string!
+            return null;
+        }
         String[] fields =  queryString.split("&");
         for (String field : fields) {
             Log.logDebug(field);
-            int i = field.indexOf('=');
-            String key = field.substring(0, i).toLowerCase();
-            String value = field.substring(i+1);
-            List<String> list;
-            if (params.containsKey(key)) {
-                list = params.get(key);
-            } else {
-                list = new ArrayList<String>();
-                params.put(key,  list);
+            try {
+                int i = field.indexOf('=');
+                String key = (i > 0 ? field.substring(0, i).toLowerCase() : field);
+                String value = (i > 0 ? field.substring(i+1) : null);
+                List<String> list;
+                if (params.containsKey(key)) {
+                    list = params.get(key);
+                } else {
+                    list = new ArrayList<String>();
+                    params.put(key,  list);
+                }
+                list.add(value);
+            } catch (IndexOutOfBoundsException ex) {
+                // ignore invalid parameters
+                Log.logWarning("invalid parameter: "+field);
             }
-            list.add(value);
         }
         return params;        
     }
